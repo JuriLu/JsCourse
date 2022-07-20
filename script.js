04b415be58fe553f17bba7e9cb304fba1fce4f6e
Jurgen Lubonja <lubonjajurgen1@gmail.com>
2022-07-20 15:07:13 +0200

    Simple array methods

--- script.js
+++ script.js
@@ -6,31 +6,31 @@
 
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
+    owner: 'Jonas Schmedtmann',
+    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+    interestRate: 1.2, // %
+    pin: 1111,
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
+    owner: 'Jessica Davis',
+    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+    interestRate: 1.5,
+    pin: 2222,
 };
 
 const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
+    owner: 'Steven Thomas Williams',
+    movements: [200, -200, 340, -300, -20, 50, 400, -460],
+    interestRate: 0.7,
+    pin: 3333,
 };
 
 const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
+    owner: 'Sarah Smith',
+    movements: [430, 1000, 700, 50, 90],
+    interestRate: 1,
+    pin: 4444,
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -66,11 +66,51 @@ const inputClosePin = document.querySelector('.form__input--pin');
 // LECTURES
 
 const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
+    ['USD', 'United States dollar'],
+    ['EUR', 'Euro'],
+    ['GBP', 'Pound sterling'],
 ]);
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+let arr = ['a', 'b', 'c', 'd', 'e']
+
+// SLICE Method
+console.log(arr.slice(2));      // Start from 2 till the end, Return a new array
+console.log(arr.slice(2, 4));   // from position 2 to 4
+console.log(arr.slice(-2));     // last two
+console.log(arr.slice(-1));     // last one
+console.log(arr.slice(1, -1));  // from second to last without the last
+console.log(arr.slice())        // Identical for returning a copy of an array
+console.log([...arr])           // Identical for returning a copy of an array
+
+// SPLICE Method
+// console.log(arr.splice(2))    // Splice changes the original array
+arr.splice(-1) ;  // Deletes the last element of the array
+console.log('Last el deleted',arr)
+arr.splice(1,2) // At position number one and deletes 2 elements
+console.log('B and C Deleted',arr)
+
+// REVERSE Method
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j','i','h','g','f'];
+console.log(arr2.reverse())        // Mutates the original array
+console.log(arr2);
+
+// CONCAT Method
+const letters = arr.concat(arr2);  // Mutates the original array
+console.log(letters);
+console.log([...arr, ...arr2])     // Same logic
+
+// JOIN Method  Creates a String with the elements of the array, with the separator specified
+console.log(letters.join('-'))
+
+//  Other Methods:
+// Push
+// Unshift
+// Pop
+// Shift
+// indexOf
+// includes
