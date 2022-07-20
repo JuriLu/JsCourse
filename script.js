5381133ce2558672d266d5c553a65ee4545fef4c
Jurgen Lubonja <lubonjajurgen1@gmail.com>
2022-07-20 17:08:54 +0200

    Banking App Phase 1

--- script.js
+++ script.js
@@ -6,17 +6,12 @@
 
 // Data
 const account1 = {
-    owner: 'Jonas Schmedtmann',
-    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-    interestRate: 1.2, // %
+    owner: 'Jonas Schmedtmann', movements: [200, 450, -400, 3000, -650, -130, 70, 1300], interestRate: 1.2, // %
     pin: 1111,
 };
 
 const account2 = {
-    owner: 'Jessica Davis',
-    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-    interestRate: 1.5,
-    pin: 2222,
+    owner: 'Jessica Davis', movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30], interestRate: 1.5, pin: 2222,
 };
 
 const account3 = {
@@ -27,10 +22,7 @@ const account3 = {
 };
 
 const account4 = {
-    owner: 'Sarah Smith',
-    movements: [430, 1000, 700, 50, 90],
-    interestRate: 1,
-    pin: 4444,
+    owner: 'Sarah Smith', movements: [430, 1000, 700, 50, 90], interestRate: 1, pin: 4444,
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -61,10 +53,27 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+    containerMovements.innerHTML = '';
+
+    movements.forEach(function (mov, i) {
+        const type = mov > 0 ? 'deposit' : 'withdrawal'
+
+        const html = `
+         <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+          <div class="movements__value">${mov}</div>
+        </div> `;
+
+        containerMovements.insertAdjacentHTML('afterbegin', html);
+    })
+}
+
+displayMovements(account1.movements)
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
-
 
 
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
@@ -149,21 +158,21 @@ const inputClosePin = document.querySelector('.form__input--pin');
 // });
 
 // // FOR EACH IN MAPS
-// const currencies = new Map([
-//     ['USD', 'United States dollar'],
-//     ['EUR', 'Euro'],
-//     ['GBP', 'Pound sterling'],
-// ]);
-//
-// currencies.forEach(function (value, key, map){
-//     console.log(`${key}: ${value}`)
-// })
-//
-//
-// const currenciesUnique = new Set(['USD','GBP','USD','EUR','EUR'])
-// console.log(currenciesUnique);
-//
-// //  _ is a throwAway variable Completely unnecessary variable
-// currenciesUnique.forEach(function (value, _, map) {
-//     console.log(`${key}: ${value}`)
-// })
+// // const currencies = new Map([
+// //     ['USD', 'United States dollar'],
+// //     ['EUR', 'Euro'],
+// //     ['GBP', 'Pound sterling'],
+// // ]);
+// //
+// // currencies.forEach(function (value, key, map){
+// //     console.log(`${key}: ${value}`)
+// // })
+// //
+// //
+// // const currenciesUnique = new Set(['USD','GBP','USD','EUR','EUR'])
+// // console.log(currenciesUnique);
+// //
+// // //  _ is a throwAway variable Completely unnecessary variable
+// // currenciesUnique.forEach(function (value, _, map) {
+// //     console.log(`${key}: ${value}`)
+// // })
