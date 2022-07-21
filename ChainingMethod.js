0c711b97cbcbdeeae1bfa67b7a2c7e10f51d2f20
Jurgen Lubonja <lubonjajurgen1@gmail.com>
2022-07-21 14:57:03 +0200

    Chaining Method

---
+++ ChainingMethod.js
@@ -0,0 +1,17 @@
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300]
+
+const eurToUsd = 1.1
+//PIPELINE
+const totalDepositsUSD = movements
+    .filter(mov => mov > 0)
+    .map((mov,i,arr) => {
+        // console.log(arr)
+       return mov * eurToUsd
+        }
+    )
+    .reduce((acc, mov,i,arr) => {
+        // console.log(arr);
+        return acc + mov
+        }, 0);
+
+console.log(totalDepositsUSD);
