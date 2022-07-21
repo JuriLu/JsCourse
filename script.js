7a484f2f8dab4a81f47b116cc27425419249ab71
Jurgen Lubonja <lubonjajurgen1@gmail.com>
2022-07-21 09:38:40 +0200

    Coding Challenge 1

--- script.js
+++ script.js
@@ -75,104 +75,38 @@ displayMovements(account1.movements)
 /////////////////////////////////////////////////
 // LECTURES
 
+// Coding Challenge #1
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const Julias = [9, 16, 6, 8, 31];
+const Kates = [10, 5, 6, 1, 4];
 
-/////////////////////////////////////////////////
+console.log(Object.keys({Julias})[0]) // get the name of the variable
 
-// let arr = ['a', 'b', 'c', 'd', 'e']
-//
-// // SLICE Method
-// console.log(arr.slice(2));      // Start from 2 till the end, Return a new array
-// console.log(arr.slice(2, 4));   // from position 2 to 4
-// console.log(arr.slice(-2));     // last two
-// console.log(arr.slice(-1));     // last one
-// console.log(arr.slice(1, -1));  // from second to last without the last
-// console.log(arr.slice())        // Identical for returning a copy of an array
-// console.log([...arr])           // Identical for returning a copy of an array
-//
-// // SPLICE Method
-// // console.log(arr.splice(2))    // Splice changes the original array
-// arr.splice(-1) ;  // Deletes the last element of the array
-// console.log('Last el deleted',arr)
-// arr.splice(1,2) // At position number one and deletes 2 elements
-// console.log('B and C Deleted',arr)
-//
-// // REVERSE Method
-// arr = ['a', 'b', 'c', 'd', 'e'];
-// const arr2 = ['j','i','h','g','f'];
-// console.log(arr2.reverse())        // Mutates the original array
-// console.log(arr2);
-//
-// // CONCAT Method
-// const letters = arr.concat(arr2);  // Mutates the original array
-// console.log(letters);
-// console.log([...arr, ...arr2])     // Same logic
-//
-// // JOIN Method  Creates a String with the elements of the array, with the separator specified
-// console.log(letters.join('-'))
-//
-// //  Other Methods:
-// // Push
-// // Unshift
-// // Pop
-// // Shift
-// // indexOf
-// // includes
+const checkDogs = function (dogsJulia, dogsKate) {
+
+    const dogsJuliaCorr = dogsJulia.slice()
+    dogsJuliaCorr.splice(0, 1);
+    dogsJuliaCorr.splice(-2);
+    console.log(dogsJuliaCorr);
+
+    const dogs = dogsJuliaCorr.concat(dogsKate)
+    console.log(dogs)
+
+    dogs.forEach(function (dog, i, arr) {
+        if (dog >= 3) {
+            console.log(`Dog number ${i + 1} is an adult and is ${dog} years old`)
+        } else {
+            console.log(`Dog ${i + 1} is a puppy with age: ${arr.at(i)} years old`)
+        }
+
+    })
+    console.log(`-----------------------`)
+}
+
+checkDogs(Julias, Kates)
 
 
-// // ES2022 Latest methods
-//
-// const arr = [23, 11, 64]
-// console.log(arr[0])                 // Same functionality , Old version
-// console.log(arr.at(0))        // Same functionality, New Version
-//
-// // Get the last element, but Say we don't know the length of the array
-// console.log(arr[arr.length - 1])   // Old Version
-// console.log(arr.slice(-1)[0])     // Old Version
-// console.log(arr.at(-1))    // New Version
-//
-// // Works with strings too
-// console.log('jonas'.at(-1))
-
-// FOR EACH LOOP
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-//
-// for (const [i,movement] of movements.entries()) {
-//     if (movement > 0) {
-//         console.log(`Movement ${i+1} You deposited ${movement}`)
-//     } else {
-//         console.log(`Movement ${i+1} You Withdrew ${Math.abs(movement)}`)
-//     }
-// }
-// console.log("----------------- FOR EACH ----------------")
-//
-//
-// //The name doesn't matter but the order does   first is the element of the array | Second is always the index | Third is the array
-// movements.forEach( function (mov,i,arr){
-//     if (mov > 0) {
-//         console.log(`Movement ${i+1} You deposited ${mov}`)
-//     } else {
-//         console.log(`Movement ${i+1} You Withdrew ${Math.abs(mov)}`)
-//     }
-// });
-
-// // FOR EACH IN MAPS
-// // const currencies = new Map([
-// //     ['USD', 'United States dollar'],
-// //     ['EUR', 'Euro'],
-// //     ['GBP', 'Pound sterling'],
-// // ]);
-// //
-// // currencies.forEach(function (value, key, map){
-// //     console.log(`${key}: ${value}`)
-// // })
-// //
-// //
-// // const currenciesUnique = new Set(['USD','GBP','USD','EUR','EUR'])
-// // console.log(currenciesUnique);
-// //
-// // //  _ is a throwAway variable Completely unnecessary variable
-// // currenciesUnique.forEach(function (value, _, map) {
-// //     console.log(`${key}: ${value}`)
-// // })
+// TEST DATA1:Julia's data[3,5,2,12,71,Kate's
+// data[4,1,15,8,31
+// TEST DATA2:Julia's data[9,16,6,8,31,Kate's
+// data[10,5,6,1,4]
